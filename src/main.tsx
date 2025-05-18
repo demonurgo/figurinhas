8c58c5c439a851da491f7214809f9d3b3ffd77d0
demonurgo <pedroaugusto.cg19@gmail.com>
2025-05-17 21:52:26 -0300

    feat: Adicionar suporte para análise com @vercel/analytics e atualizar dependências

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { SpeedInsights } from "@vercel/speed-insights/react"
+import { Analytics } from "@vercel/analytics/react"
 
 // Registra o service worker para a funcionalidade PWA
 if ('serviceWorker' in navigator) {
@@ -20,5 +21,6 @@ createRoot(document.getElementById("root")!).render(
   <>
     <App />
     <SpeedInsights />
+    <Analytics />
   </>
 );
